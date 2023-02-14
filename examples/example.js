3794332615332c41843002cc06a48d36e4503672
jamm <joseangel.montesmeana@gmail.com>
2023-02-14 11:08:14 +0100

    Add full example

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,5 @@
 /*
- * Latch NodeJS SDK Example
+ * Latch NodeJS SDK
  * Copyright (C) 2014 Eleven Paths
 
  * This library is free software; you can redistribute it and/or
@@ -17,52 +17,127 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-var latch = require('../index.js');
 
-latch.init({ appId: 'MY_APPID', secretKey: 'MY_SECRETKEY' });
-var response = latch.status('MY_ACCOUNTID', function(err, result) {
-    if (err) {
-        console.log(util.inspect(err, { showHidden: true, depth: null, colors:true }));
-    } else {
-        console.log(util.inspect(result, { showHidden: true, depth: null, colors: true }));
-    }
-});
+// To run an example just fill in the value of the constants and run the example.
+
+const latch = require('../index.js');
+const util = require('util');
 
 
-/* // PAIR EXAMPLE WITH WEB3
-let MY_ACCOUNTID = "<MY_ACCOUNTID>"
-let MY_APPID = "<MY_APPID>"
-let MY_SECRETKEY = "<MY_SECRETKEY>"
+// Mandatory
+const MY_APPID = "<MY_APPID>";
+const MY_SECRETKEY = "<MY_SECRETKEY>";
 
-let WEB3WALLET = "<WEB3WALLET>"
-let WEB3SIGNATURE = "<WEB3SIGNATURE>"
+// Mandatory depending on the method
+const MY_ACCOUNTID = "<MY_ACCOUNTID>"
+const MY_ACCOUNT_NAME = "<MY_ACCOUNT_NAME>"
+const PAIRING_CODE = "<PAIRING_CODE>"
 
-latch.init({ appId: MY_APPID, secretKey: MY_SECRETKEY });
-
-latch.pairWithId(MY_ACCOUNTID, function(err, data) {
-    if (data["data"]["accountId"]) {
-        console.log(data["data"]["accountId"]);
-    } else if (data["error"]) {
-        console.log("Error");
-    }
-}, WEB3WALLET, WEB3SIGNATURE);
-
-
-// PAIR EXAMPLE WITH WEB3 USING PAIRING_CODE
-let MY_APPID = "<MY_APPID>"
-let MY_SECRETKEY = "<MY_SECRETKEY>"
-
-let WEB3WALLET = "<WEB3WALLET>"
-let WEB3SIGNATURE = "<WEB3SIGNATURE>"
-let PAIRING_CODE = "<PAIRING_CODE>"
+// Mandatory for web3
+const WEB3WALLET = "<WEB3WALLET>"
+const WEB3SIGNATURE = "<WEB3SIGNATURE>"
 
 latch.init({ appId: MY_APPID, secretKey: MY_SECRETKEY });
 
-latch.pair(PAIRING_CODE, function(err, data) {
-    if (data["data"]["accountId"]) {
-        console.log(data["data"]["accountId"]);
-    } else if (data["error"]) {
-        console.log("Error");
-    }
-}, WEB3WALLET, WEB3SIGNATURE);
- */
+
+// USING THE SDK IN NODEJS
+
+// PAIR WITH ID (FOR TESTING)
+function example_pair_with_id() {
+    let response = latch.pairWithId(MY_ACCOUNT_NAME, function(err, result) {
+        if (err) {
+            console.log(util.inspect(err, { showHidden: true, depth: null, colors:true }));
+        } else {
+            console.log(util.inspect(result, { showHidden: true, depth: null, colors: true }));
+        }
+    });
+}
+
+// PAIR WITH PAIRING CODE
+function example_pair_with_paring_code() {
+    let response = latch.pair(PAIRING_CODE, function (err, result) {
+        if (err) {
+            console.log(util.inspect(err, {showHidden: true, depth: null, colors: true}));
+        } else {
+            console.log(util.inspect(result, {showHidden: true, depth: null, colors: true}));
+        }
+    });
+}
+
+
+// USING THE SDK IN NODEJS FOR WEB3 SERVICES
+
+// PAIR WITH ID (WEB3)
+function example_pair_with_id_web3() {
+    let response = latch.pairWithId(MY_ACCOUNT_NAME, function (err, result) {
+        if (err) {
+            console.log(util.inspect(err, {showHidden: true, depth: null, colors: true}));
+        } else {
+            console.log(util.inspect(result, {showHidden: true, depth: null, colors: true}));
+        }
+    }, WEB3WALLET, WEB3SIGNATURE);
+}
+
+// PAIR WITH PAIRING CODE (WEB3)
+function example_pair_with_paring_code_web3() {
+    let response = latch.pair(PAIRING_CODE, function (err, result) {
+        if (err) {
+            console.log(util.inspect(err, {showHidden: true, depth: null, colors: true}));
+        } else {
+            console.log(util.inspect(result, {showHidden: true, depth: null, colors: true}));
+        }
+    }, WEB3WALLET, WEB3SIGNATURE);
+}
+
+// USING THE SDK IN NODEJS FOR ALL (WEB3 AND NOT WEB3)
+
+// GET STATUS
+function example_get_status() {
+    let response = latch.status(MY_ACCOUNTID, null, null, function (err, result) {
+        if (err) {
+            console.log(util.inspect(err, {showHidden: true, depth: null, colors: true}));
+        } else {
+            console.log(util.inspect(result, {showHidden: true, depth: null, colors: true}));
+        }
+    });
+}
+
+// LOCK STATUS
+function example_lock() {
+    let response = latch.lock(MY_ACCOUNTID, null, function (err, result) {
+        if (err) {
+            console.log(util.inspect(err, {showHidden: true, depth: null, colors: true}));
+        } else {
+            console.log(util.inspect(result, {showHidden: true, depth: null, colors: true}));
+        }
+    });
+}
+
+// UNLOCK STATUS
+function example_unlock() {
+    let response = latch.unlock(MY_ACCOUNTID, null, function (err, result) {
+        if (err) {
+            console.log(util.inspect(err, {showHidden: true, depth: null, colors: true}));
+        } else {
+            console.log(util.inspect(result, {showHidden: true, depth: null, colors: true}));
+        }
+    });
+}
+
+// UNPAIR
+function example_unpair() {
+    let response = latch.unpair(MY_ACCOUNTID, function (err, result) {
+        if (err) {
+            console.log(util.inspect(err, {showHidden: true, depth: null, colors: true}));
+        } else {
+            console.log(util.inspect(result, {showHidden: true, depth: null, colors: true}));
+        }
+    });
+}
+
+
+// TO RUN EXAMPLE
+example_pair_with_id_web3()
+example_get_status()
+example_lock()
+example_unpair()
